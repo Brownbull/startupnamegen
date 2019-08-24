923965fa5a88c150e118290764df1a4de1979f32
Carcamo <Gabriel.Carcamo@experian.com>
2019-08-24 17:50:26 -0400

    generating one random word

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:english_words/english_words.dart' as prefix0;
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context){ 
+  Widget build(BuildContext context){
+    // final wordPair = WordPair.random();  
     return MaterialApp(
       title: 'Welcome to Flutter Mobile Dev',
       home: Scaffold(
@@ -12,9 +15,53 @@ class MyApp extends StatelessWidget{
           title: Text('Welcome 2 Flutter'),
         ),
         body: Center(
-          child: Text('Hello Mobile World'),
+          // child: Text('Hello Mobile World'),
+          // child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         )
       )
     );
   }
 }
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context){
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+
+  @override
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      )
+    );
+  }
+
+  @override
+  Widget _buildSuggestions(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i){
+        if(i.isOdd) return Divider();
+
+        final index = i ~/ 2;
+        if(index >= _suggestions.length){
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
